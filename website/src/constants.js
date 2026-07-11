40f66847698872138b806b78e62f31c75200271b
Thi <dinhanhthi@gmail.com>
2026-07-11 22:52:19 +0200

    Update Pro link + werbsite polish

--- website/src/constants.js
+++ website/src/constants.js
@@ -2,5 +2,5 @@ export const GITHUB_URL = "https://github.com/dinhanhthi/PopGuy";
 export const RELEASES_URL = `${GITHUB_URL}/releases`;
 export const DOWNLOAD_URL = `${RELEASES_URL}/latest`;
 export const PRO_CHECKOUT_URL =
-  "https://dinhanhthi.lemonsqueezy.com/checkout/buy/d2dd34c0-d3c7-4be8-9b17-b3f2cbf6c5f8";
+  "https://dinhanhthi.lemonsqueezy.com/checkout/buy/ea521c18-178e-4aaa-a479-af053e38285e";
 
